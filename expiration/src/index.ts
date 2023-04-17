a5101672cdea97bcaf8268cb727472c720214f61
wjansen23 <wjansen23@verizon.net>
2023-04-17 17:16:12 -0400

    Remaining deployments

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -3,6 +3,7 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 
 const start = async () =>{
     //Check env variables
+    console.log("Starting up expiration service....");
     if (!process.env.NATS_CLIENT_ID){
         throw new Error('NATS_CLIENT_ID must be defined.')
     }
