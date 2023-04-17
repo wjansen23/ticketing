a4db031f5acabe9e63edcb05a5d3494d455d514b
wjansen23 <wjansen23@verizon.net>
2023-04-17 17:23:56 -0400

    minor changes

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,7 +5,7 @@ import { OrderCancelledListener } from './events/listeners/order-cancelled-liste
 import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const start = async () =>{
-    console.log("Starting up payments service....");
+    console.log("Starting up payments service...");
     //Check env variables
     if (!process.env.JWT_KEY){
         throw new Error('JWT_KEY must be defined');
